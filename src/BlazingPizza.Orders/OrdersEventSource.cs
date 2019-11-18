82b2fdeb3829aa166d64372db7b8435f3a228942
Sourabh Shirhatti <soshir@microsoft.com>
2019-11-18 13:11:09 -0800

    Add total orders counter

---
+++ src/BlazingPizza.Orders/OrdersEventSource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Orders
+{
+    internal sealed class OrdersEventSource : EventSource
+    {
+        public static readonly OrdersEventSource Log = new OrdersEventSource();
+
+        private PollingCounter _totalOrdersCounter;
+
+        private long _totalOrders;
+
+        internal OrdersEventSource()
+            : base("BlazingOrders.Pizza")
+        {
+
+        }
+
+        internal void OrderCreated()
+        {
+            Interlocked.Increment(ref _totalOrders);
+        }
+
+        protected override void OnEventCommand(EventCommandEventArgs command)
+        {
+            if (command.Command == EventCommand.Enable)
+            {
+                _totalOrdersCounter ??= new PollingCounter("total-orders", this, () => _totalOrders)
+                {
+                    DisplayName = "Total Orders"
+                };
+            }
+        }
+    }
+}
